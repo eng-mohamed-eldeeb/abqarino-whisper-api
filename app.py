1d962aa3930a0e8c97ea00d7e678cd791f3dc5df
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-06-24 17:29:20 +0300

    Add whisper

--- app.py
+++ app.py
@@ -19,13 +19,21 @@
 #     socketio.run(app, host='localhost', port=5000)
 # # will this python code work?
 
+import whisper
+import sys
 import os
 from flask import Flask
 
 app = Flask(__name__)
+model = whisper.load_model("small")
 env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
-@app.route("/")
+@app.route('/')
 def index():
- return "Hello World!"
+    model = whisper.load_model("small")
+    result = model.transcribe("audio.mp3")
+    return ( result["text"].encode('utf-8').decode(sys.stdout.encoding))
+
+if __name__ == '__main__':
+    app.run()
