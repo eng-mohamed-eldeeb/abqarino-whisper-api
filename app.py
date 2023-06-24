ce35e61fa944e2f0cea23a993393bdb244676438
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-06-24 20:09:17 +0300

    Comment the data

--- app.py
+++ app.py
@@ -19,13 +19,13 @@
 #     socketio.run(app, host='localhost', port=5000)
 # # will this python code work?
 
-import whisper
-import sys
+# import whisper
+# import sys
 import os
 from flask import Flask
 
 app = Flask(__name__)
-model = whisper.load_model("small")
+# model = whisper.load_model("small")
 env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
