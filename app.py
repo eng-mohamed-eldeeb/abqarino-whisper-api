8e89f3c852ca17a43945afc332e1dbe9cfe06233
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-06-24 15:09:26 +0300

    Add code

---
+++ app.py
@@ -0,0 +1,31 @@
+# import whisper
+# import sys
+# from flask import Flask
+# from flask_socketio import SocketIO, emit
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'your-secret-key'
+
+# socketio = SocketIO(app, cors_allowed_origins="*")
+
+# @app.route('/recpy')
+# def index():
+#     model = whisper.load_model("small")
+#     result = model.transcribe("audio.mp3")
+#     return result["text"].encode('utf-8').decode(sys.stdout.encoding)
+
+
+# if __name__ == '__main__':
+#     socketio.run(app, host='localhost', port=5000)
+# # will this python code work?
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+
+@app.route("/")
+def index():
+ return "Hello World!"
